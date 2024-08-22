82d44390a389eac45caac3a8241117767b10f71f
aeoniaa <100823361+aeoniaa@users.noreply.github.com>
2024-08-22 22:06:35 +0800

    Restructured the app, Returns "hello world" on running
    Can run the app with <flask --app main run> which returns a simple paragraph

---
+++ app/main/routes.py
@@ -0,0 +1,11 @@
+"""Main route views"""
+
+from flask import Blueprint, render_template, flash, redirect, url_for, request, session
+
+main_bp = Blueprint('main_bp', __name__)
+
+@main_bp.route("/")
+def home_page():
+    """The landing page"""
+    # return render_template('home.html')
+    return "<p>Hello, World!</p>"
--- app/routes.py
+++
@@ -1,2 +0,0 @@
-from flask import Flask, render_template, flash, redirect, url_for, request, session
-from app import app
