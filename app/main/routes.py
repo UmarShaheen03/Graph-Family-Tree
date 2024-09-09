65c2068d389f5c37e0ea8898a4851b766487ec7f
ann250902 <150408221+ann250902@users.noreply.github.com>
2024-09-09 20:07:12 +0800

    Add biography editing and comment submission functionality

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,10 @@
 """Main route views"""
 
 from flask import Blueprint, render_template, flash, redirect, url_for, request, session
-
+from forms import BiographyEditForm, CommentForm
+from models import Biography,Comment
+from datetime import datetime
+from flask_login import login_required, current_user
 main_bp = Blueprint('main_bp', __name__)
 
 @main_bp.route("/")
@@ -26,4 +29,24 @@ def biography_page():
 
 @main_bp.route("/submit_comment")
 def submit_comment():
+    return render_template('biography.html')
+
+@login_required
+def biography(biography_id):
+    biography = Biography.query.get_or_404(biography_id)
+    comments = Comment.query.filter_by(biography_id = biography_id).all()
+    if BiographyEditForm().validate_on_submit and current_user.is_manager:
+        BiographyEditForm.populate_obj(biography)
+        db.session.commit()
+        flash('Biography page updated successfully')
+    if CommentForm().validate_on_submit():
+        new_comment = Commnent(
+            user_id=current_user.id,
+            text=CommentForm().comment.data,
+            timestamp=datetime.now()
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+        flash('Comments added successfully')
+        return(redirect(url_for('biography')))
     return render_template('biography.html')
