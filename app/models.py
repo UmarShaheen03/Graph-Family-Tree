74b8578749eac2aa17d7ce4bf85f2c5ed953ce33
ann250902 <150408221+ann250902@users.noreply.github.com>
2024-09-09 19:15:48 +0800

    Define the models for biography and comment

--- app/models.py
+++ app/models.py
@@ -7,3 +7,21 @@ from app import db
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
+
+class Biography(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    dob = db.Column(db.String(50), nullable=True)
+    biography = db.Column(db.Text, nullable=True)
+    location = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(100), nullable=True)
+    phonenumber = db.Column(db.Integer, nullable=True)
+    address = db.Column(db.String(100), nullable=True)
+
+
+class Comment(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    node_id = db.Column(db.Integer, db.ForeignKey('node.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    text = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime)
