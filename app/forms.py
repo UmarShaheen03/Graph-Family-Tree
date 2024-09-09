0d924f69dbf0c8b844092bd1867a633b53518788
ann250902 <150408221+ann250902@users.noreply.github.com>
2024-09-09 19:53:52 +0800

    Add forms for submitting comments and editing biography details

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Optional
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -18,6 +18,16 @@ class RegistrationForm(FlaskForm):
 
 class AddNodeForm(FlaskForm):
     ####
+class CommentForm(FlaskForm):
+    comment=TextAreaField('Comment', validators=[DataRequired()])
+    submit=SubmitField('Submit')
 
-
-    
+class BiographyEditForm(FlaskForm):
+    name=StringField('Full Name', validators=[DataRequired()])
+    dob =StringField('Date of Birth', validators=[Optional()])
+    biography=StringField('Biography', validators=[Optional()])
+    location=StringField('Location', validators=[Optional()])
+    email=StringField('Email', validators=[Optional()])
+    phonenumber=StringField('Phone Number', validators=[Optional()])
+    address=StringField('Address', validators=[Optional()])
+    submit=SubmitField
