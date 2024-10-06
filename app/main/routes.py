9cb9eafec8eb9d7350a2aa37ee37f4cb65bebce6
Umar Shaheen <umar.shaheen93@gmail.com>
2024-10-06 14:26:27 +0800

    "Created a json request for admin priveleges to be able to display toast notification when the request has been sent"

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 """Main route views"""
 
 import os
-from flask import Blueprint, Flask, render_template, flash, redirect, url_for, request, session, send_file, send_from_directory
+from flask import Blueprint, Flask, render_template, flash, redirect, url_for, request, session, send_file, send_from_directory, jsonify
 from app.forms import LoginForm, SignupForm, ForgotPassword, ResetPassword, AddNodeForm, UpdateNode, AppendGraph, BiographyEditForm, CommentForm, Search_Node
 from app.models import Biography, Comment
 from app.accounts import signup, login, SignupError, LoginError, init_database, reset_email, verify_reset, reset, send_email_to_admin, make_user_admin
@@ -450,7 +450,7 @@ def request_admin():
 
     send_email_to_admin(token)
 
-    return "Admin privileges requested! Please wait for admin approval."
+    return jsonify({"message": user_email})
 
 
 @main_bp.route('/approve_admin', methods=['GET'])
